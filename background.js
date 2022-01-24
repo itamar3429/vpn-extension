1c758e41356f714fa976de8a2e3f2eb621d2d33a
itamar3429 <itamar3429@gmail.com>
2022-01-24 16:07:58 +0200

    fixed webrequest authenication

--- background.js
+++ background.js
@@ -1,1 +1,40 @@
-console.log('success')
+window.onload = async () => {
+
+    const set_credentials = async (opt = {
+        username,
+        password
+    }, sender) => {
+        console.log(sender.url);
+        if (opt.username && opt.password) {
+            await chrome.webRequest.onAuthRequired.addListener((details) => {
+                    console.log("onAuthRequired!", details);
+                    let credentials = {
+                        username: opt.username,
+                        password: opt.password
+                    }
+                    console.log('setting credentials');
+                    return {
+                        authCredentials: credentials
+                    }
+
+                }, {
+                    urls: ["<all_urls>"]
+                },
+                ['blocking']
+            );
+            chrome.webRequest.onCompleted.addListener(
+                () => {
+                    console.log('completed credentials')
+                }, {
+                    urls: ["<all_urls>"]
+                }
+            );
+        }
+    }
+
+    await chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+        console.log(message.data)
+        await set_credentials(message.data, sender)
+        sendResponse('messgae received');
+    });
+}
--- popup.js
+++ popup.js
@@ -19,95 +19,190 @@ window.onload = async () => {
         document.getElementById('alert').classList.add('showalert')
         document.getElementById('alert').classList.add('alertop')
         setTimeout(() => {
-            if (alertQ == alertQT) { document.getElementById('alert').classList.remove('alertop') }
+            if (alertQ == alertQT) {
+                document.getElementById('alert').classList.remove('alertop')
+            }
         }, 7000)
         setTimeout(() => {
-            if (alertQ == alertQT) { document.getElementById('alert').classList.remove('showalert') }
+            if (alertQ == alertQT) {
+                document.getElementById('alert').classList.remove('showalert')
+            }
         }, 9000)
     }
 
-    await get_storage('proxy_list', async (val) => {
-        console.log(val);
-        let result = null
-        result = val
-        if (result && result.length) {
-            try {
-                let options = ''
-                result.forEach(re => {
-                    let s = re.last ? 'selected' : ''
-                    options += `<option value="${JSON.stringify(re).replaceAll('"', "'")}" id="ipOption" ${s}>${re.ip}:${re.port}</option>`
-                });
-                document.getElementById('savedSelect').innerHTML = options
+    const loadF = async (cb) => {
+        await new Promise(async (res) => {
+            await get_storage('proxy_list', (val) => {
+                console.log(val);
+                let result = null
+                result = val
+                if (result && result.length) {
+                    try {
+                        let options = `<option value="null" id="optionss" selected >select proxy</option>`
+                        result.forEach(re => {
+                            let s = re.last ? 'selected' : ''
+                            if (s)
+                                options.replaceAll('selected', '')
+                            options += `<option value="${JSON.stringify(re).replaceAll('"', "'")}" id="ipOption" ${s}>${re.ip}:${re.port}</option>`
+                        });
+                        document.getElementById('savedSelect').innerHTML = options
 
-                document.querySelector('#savedSelect').addEventListener('change', async (e) => {
-                    let proxy = JSON.parse(e.target.value.replaceAll("'", '"'))
-                    document.querySelector('#ip').value = proxy.ip
-                    document.querySelector('#port').value = proxy.port
-                    document.querySelector('#username').value = proxy.username
-                    document.querySelector('#password').value = proxy.password
-                    setalert(`proxy changed`)
-                })
-            } catch (err) {
-                console.log(err);
-            }
-            if (result.filter(x => x.last === true).length > 0)
-                result = result.filter(x => x.last === true)[0]
-            else
-                result = result[0]
-            document.querySelector('#ip').value = result.ip
-            document.querySelector('#port').value = result.port
-            document.querySelector('#username').value = result.username
-            document.querySelector('#password').value = result.password
-        } else {
-            options = `<option value="0" id="optionss">no proxy saved</option>`
-            document.getElementById('savedSelect').innerHTML = options
-        }
-    })
+                        document.querySelector('#savedSelect').addEventListener('change', async (e) => {
 
-    const setProxy = async (opt = { ip, port, username, password, singleProxy }) => {
-        try {
-            await chrome.proxy.settings.clear({});
-            await chrome.webRequest.onAuthRequired.addListener(
-                function (details, callbackFn) {
-                    console.log("onAuthRequired!", details, callbackFn);
-                    callbackFn({
-                        authCredentials: { username, password }
-                    });
-                },
-                { urls: ["<all_urls>"] },
-                ['asyncBlocking']
-            );
-            var config = null
-            if (opt.ip && opt.port) {
-                config = {
-                    mode: "fixed_servers",
-                    rules: {
-                        singleProxy: {
-                            host: opt.ip,
-                            port: opt.port ? parseInt(opt.port) : null,
-                        }
+                            if (e.target.value != 'null') {
+                                let proxy = JSON.parse(e.target.value.replaceAll("'", '"'))
+                                document.querySelector('#ip').value = proxy.ip
+                                document.querySelector('#port').value = proxy.port
+                                document.querySelector('#username').value = proxy.username
+                                document.querySelector('#password').value = proxy.password
+                                setalert(`proxy changed, for the change to take affect please press set`)
+                            } else {
+                                setalert(`select proxy`)
+                                document.querySelector('#ip').value = ''
+                                document.querySelector('#port').value = ''
+                                document.querySelector('#username').value = ''
+                                document.querySelector('#password').value = ''
+                            }
+                        })
+                    } catch (err) {
+                        console.log(err);
                     }
-                };
-                await chrome.proxy.settings.set({ value: config, scope: 'regular' });
-                setalert(`proxy set to: ${opt.ip}:${opt.port}`)
-            } else setalert(`please select ip and port`)
+                    if (result.filter(x => x.last === true).length > 0)
+                        result = result.filter(x => x.last === true)[0]
+                    else
+                        result = result[0]
+                    document.querySelector('#ip').value = result.ip
+                    document.querySelector('#port').value = result.port
+                    document.querySelector('#username').value = result.username
+                    document.querySelector('#password').value = result.password
+                } else {
+                    options = `<option value="null" id="optionss">no proxy saved</option>`
+                    document.getElementById('savedSelect').innerHTML = options
+                }
+                res(true)
+            })
+        }).then(async () => {
+            await chrome.proxy.settings.get({
+                    'incognito': false
+                },
+                function (config) {
+                    if (config.value.rules && config.value.rules.singleProxy) {
+                        let d = config.value.rules.singleProxy
+                        document.getElementById('currentProxy').innerText = `proxy is listening on:\n  ${d.host} : ${d.port}`
+                    } else {
+                        document.getElementById('currentProxy').innerText = `proxy is not active`
+                    }
+                }
+            );
+            if (cb) {
+                await cb()
+            }
+        })
+
+
+
+    }
+
+    await loadF()
+
+    const setProxy = async (opt = {
+        ip,
+        port,
+        username,
+        password,
+        singleProxy
+    }, cb) => {
+        try {
+            new Promise(async (resolve) => {
+                if (opt.username && opt.password) {
+
+                    // chrome.tabs.query({}, function (tabs) {
+                    //     console.log(tabs);
+                    //     console.log('log tabs');
+                    //     for (let i = 0; i < tabs.length; i++) {
+                    //         chrome.tabs.sendMessage(tabs[i].id, {
+                    //             data: opt
+                    //         }, function (response) {
+                    //             if (response && response.success)
+                    //                 setalert('listener added')
+                    //             else {
+                    //                 setalert('failed to send messgae')
+                    //             }
+                    //         });
+                    //     }
+                    // });
+                    chrome.runtime.sendMessage({
+                        data: opt
+                    }, function (response) {
+                        if (response)
+                            setalert(response)
+
+                    });
+
+                    // await chrome.webRequest.onAuthRequired.addListener((details) => {
+                    //         console.log("onAuthRequired!", details);
+                    //         let credentials = {
+                    //             username: opt.username,
+                    //             password: opt.password
+                    //         }
+                    //         return {
+                    //             authCredentials: credentials
+                    //         }
+
+                    //     }, {
+                    //         urls: ["<all_urls>"]
+                    //     },
+                    //     ['blocking']
+                    // );
+                    // chrome.webRequest.onCompleted.addListener(
+                    //     () => {
+                    //         setalert('proxy credentials set')
+                    //     }, {
+                    //         urls: ["<all_urls>"]
+                    //     }
+                    // );
+                }
+                var config = null
+                if (opt.ip && opt.port) {
+                    await chrome.proxy.settings.set({
+                        value: {
+                            mode: 'direct'
+                        },
+                        scope: 'regular'
+                    });
+                    config = {
+                        mode: "fixed_servers",
+                        rules: {
+                            singleProxy: {
+                                host: opt.ip,
+                                port: opt.port ? parseInt(opt.port) : null,
+                            }
+                        }
+                    };
+                    await chrome.proxy.settings.set({
+                        value: config,
+                        scope: 'regular'
+                    });
+                    setalert(`proxy set to: ${opt.ip}:${opt.port}`)
+                    resolve(true)
+                } else {
+                    setalert(`please select ip and port`)
+                    resolve(false)
+                }
+            }).then((val) => {
+                if (val && cb) {
+                    cb()
+                }
+            })
         } catch (error) {
             setalert(`error while setting proxy: ${error.message}`)
+            if (cb) {
+                await cb()
+            }
+            resolve()
         }
     }
 
-    chrome.proxy.settings.get(
-        { 'incognito': false },
-        function (config) {
-            if (config.value.rules) {
-                let d = config.value.rules.singleProxy
-                document.getElementById('currentProxy').innerText = `proxy is listening on:\n  ${d.host} : ${d.port}`
-            }
-            else {
-                document.getElementById('currentProxy').innerText = `proxy is not active`
-            }
-        }
-    );
 
     document.querySelector('#btnSetProxy').addEventListener('click', async (e) => {
         await get_storage('proxy_list', async (val) => {
@@ -117,35 +212,72 @@ window.onload = async () => {
             let username = document.querySelector('#username').value
             let password = document.querySelector('#password').value
             let singleProxy = document.querySelector('#single').value
-            if (val && val.length && val.filter(x => x.ip == ip && x.port == port && x.username == username && x.password == password).length == 0) { //see if i need to insert new value
-                val.map(x => x.last = false)
-                val.push({ ip, port, username, password, last: true })
-            }
-            else if (val && val.length) {
-                val.map(x => {
-                    if (x.ip == ip && x.port == port && x.username == username && x.password == password)
-                        x.last = true
-                    else
-                        x.last = false
-                })
-            } else {
-                val = [{ ip, port, username, password, last: true }]
+            if (ip && port) {
+                if (val && val.length && val.filter(x => x.ip == ip && x.port == port && x.username == username && x.password == password).length == 0) { //see if i need to insert new value
+                    val.map(x => x.last = false)
+                    val.push({
+                        ip,
+                        port,
+                        username,
+                        password,
+                        last: true
+                    })
+                } else
+                if (val && val.length) {
+                    val.map(x => {
+                        if (x.ip == ip && x.port == port && x.username == username && x.password == password)
+                            x.last = true
+                        else
+                            x.last = false
+                    })
+                } else {
+                    val = [{
+                        ip,
+                        port,
+                        username,
+                        password,
+                        last: true
+                    }]
+                }
             }
             console.log(val);
             await set_storage('proxy_list', val)
-            await setProxy({ ip, port, username, password, singleProxy })
+            new Promise(async (res) => {
+                await setProxy({
+                    ip,
+                    port,
+                    username,
+                    password,
+                    singleProxy
+                }, res)
+            }).then(async () => {
+                await loadF()
+            })
+
             // alert(`ip is : ${ip}, \n port is: ${port}, \n username is : ${username}, \n password is: ${password}`)
             console.log(`ip is : ${ip}, \n port is: ${port}, \n username is : ${username}, \n password is: ${password}`);
         })
     })
 
     document.querySelector('#btnClearProxy').addEventListener('click', async (e) => {
-        document.querySelector('#ip').value = ''
-        document.querySelector('#port').value = ''
-        document.querySelector('#username').value = ''
-        document.querySelector('#password').value = ''
-        await chrome.proxy.settings.clear({});
-        document.getElementById('currentProxy').innerText = `proxy is not active`
+        await chrome.proxy.settings.set({
+            value: {
+                mode: 'direct'
+            },
+            scope: 'regular'
+        });
+        await new Promise(async (res, rej) => {
+            await loadF(res)
+        }).then(() => {
+            document.querySelector('#ip').value = ''
+            document.querySelector('#port').value = ''
+            document.querySelector('#username').value = ''
+            document.querySelector('#password').value = ''
+        })
+        let options = document.getElementById('savedSelect').innerHTML
+        options = options.split('<option').map(x => x = '<option' + x.replaceAll('selected', '')).filter(x => !x.includes('select proxy<'))
+        options = `<option value="null" id="optionss" selected >select proxy</option>` + options.join(' ')
+        document.getElementById('savedSelect').innerHTML = options
         setalert(`proxy cleared`)
     })
 
@@ -161,15 +293,25 @@ window.onload = async () => {
                     val = val.filter(x => !(x.ip == ip && x.port == port && x.username == username && x.password == password))
                 }
                 await set_storage('proxy_list', val)
-                document.querySelector('#ip').value = ''
-                document.querySelector('#port').value = ''
-                document.querySelector('#username').value = ''
-                document.querySelector('#password').value = ''
-                await chrome.proxy.settings.clear({});
+                await new Promise(async (res, rej) => {
+                    await loadF()
+                    res(true)
+                }).then(() => {
+                    document.querySelector('#ip').value = ''
+                    document.querySelector('#port').value = ''
+                    document.querySelector('#username').value = ''
+                    document.querySelector('#password').value = ''
+                })
+
+                await chrome.proxy.settings.set({
+                    value: {
+                        mode: 'direct'
+                    },
+                    scope: 'regular'
+                });
                 setalert(`proxy deleted to deactivate current proxy press clear`)
             })
-        }
-        catch (error) {
+        } catch (error) {
             setalert(`error while deleting: ${error.message}`)
         }
     })
